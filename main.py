d8a3df99eaf7bffb6fb64fa41836effea76bb084
Deirdre Lewis <lewis.deirdre.k@gmail.com>
2019-02-27 22:43:49 -0600

    Build a blog complete. I always forget to back this up as I am working.

---
+++ main.py
@@ -0,0 +1,60 @@
+from flask import Flask, request, redirect, render_template, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:buildablog@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+app.secret_key = "super secret key"
+
+class Blog(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    blogtitle = db.Column(db.String(50))
+    content = db.Column(db.Text)
+    completed = db.Column(db.Boolean)
+
+    def __init__(self, blogtitle, content):
+        self.blogtitle = blogtitle
+        self.content = content
+
+
+blogs = []
+
+
+@app.route('/')
+def index():
+    blogs = Blog.query.all()
+    return render_template('', blogs=blogs)
+
+@app.route('/blog/<int:blog_id>')
+def blog(blog_id):
+    blog = Blog.query.filter_by(id=blog_id).one()
+
+    return render_template('blog.html', blog=blog)
+
+@app.route('/add')
+def add():
+    return render_template('newpost.html')
+
+@app.route('/newpost', methods=['POST'])
+def add_new_post():
+    blogtitle = request.form["blogtitle"]
+    content = request.form["content"]
+
+    if not blogtitle or not content:
+        flash("All fields are required. Please try again.")
+        return redirect(url_for('/newpost.html'))
+    else:
+
+        post = Blog(blogtitle=blogtitle, content=content)
+
+        db.session.add(post)
+        db.session.commit()
+        
+        flash('New entry was successfully posted!')
+        return redirect('/')
+
+if __name__ == '__main__':
+    app.run()
