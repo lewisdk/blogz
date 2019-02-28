c944ac2da5c5ad249af640b4a2b67baa18213f50
Deirdre Lewis <lewis.deirdre.k@gmail.com>
2019-02-28 10:53:57 -0600

    added validation apps

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from flask import Flask, request, redirect, render_template, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
+import cgi
+import os
+import jinja2
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader
+(template_dir), autoescape=True)
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
@@ -84,7 +91,64 @@ def signup():
             session['username'] = username
             return redirect('/')
 
+def good_username(username):
+    username = request.form['username']
 
+    if (len(username) > 3 and len(username) < 15):
+        return True
+    else:
+        return False
+
+def good_password(password):
+    password = request.form['password']
+
+    if (len(password) > 3 and len(password) < 15):
+        if (" ") in password:
+            return True
+    else:
+        return False 
+
+def password_match(verify):
+    verify = request.form['verify']
+    password = request.form['password']
+
+    if [password] == [verify]:
+        return True
+    else:
+        return False
+
+@app.route("/", methods=['POST'])
+def validate_form():
+    username = request.form['username']
+    password = request.form['password']
+    verify = request.form['verify']
+
+    username_error = ''
+    password_error = ''
+    verify_error = ''
+    email_error=''
+
+    if good_username(username) == False:
+        username_error = 'That is not a valid username.'
+        username = ''
+
+    if good_password(password) == False:
+        password_error = 'That is not a valid password.'
+        password = ''
+
+    if password_match(verify) == False:
+        verify_error = 'Passwords do not match.'
+        verify = ''
+    if not username_error and not password_error and not verify_error:
+            return redirect('/newpost')
+        else:
+            template = jinja_env.get_template('index.html')
+            return template.render(username_error=username_error, password_error=password_error, 
+                verify_error=verify_error, 
+                username = username,
+                password = '',
+                verify = '')
+                
 @app.route('/login')
 def login():
     if request.method == 'POST':
@@ -105,7 +169,10 @@ def login():
                     return('/signup')
     return render_template('login.html')
 
-#@app.route('/index')
+@app.route('/logout')
+def logout():
+    del session['username']
+    return redirect('/')
 
 if __name__ == '__main__':
     app.run()
